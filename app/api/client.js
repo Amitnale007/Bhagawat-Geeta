2ab2bbbcc29781a064b55fdc01795373b3985c90
unknown <amitnale.07@gmail.com>
2024-01-13 21:47:32 +0530

    private key removed from source code

--- app/api/client.js
+++ app/api/client.js
@@ -1,10 +1,10 @@
 import { create } from "apisauce";
-
+import config from "../config";
 const api = create({
   baseURL: "https://bhagavad-gita3.p.rapidapi.com/v2",
   headers: {
-    "X-RapidAPI-Key": "f3bb6358a0mshd341bfbef6ac552p19f94ajsnd11a44b54b93",
-    "X-RapidAPI-Host": "bhagavad-gita3.p.rapidapi.com",
+    "X-RapidAPI-Key": config.RapidAPIKey,
+    "X-RapidAPI-Host": config.RapidAPIHost,
   },
 });
 
